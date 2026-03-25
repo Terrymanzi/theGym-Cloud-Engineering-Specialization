732d105f1847ffea63c6a25774877afbea2fc540
terrymanzi <m.terry@alustudent.com>
2026-03-25 23:40:36 +0200

    fix: relative path directory creation and file touch process in exercise-1 script

--- Linux-Proficiency/exercises/exercise-1.sh
+++ Linux-Proficiency/exercises/exercise-1.sh
@@ -5,7 +5,5 @@ name="terry"
 for ((i=0; i < ${#extensions[@]}; i++))
 do
         mkdir "${extensions[i]}"
-        cd "${extensions[i]}"
-        touch "$name${extensions[i]}"
-        cd ..
+        touch "${extensions[i]}/$name${extensions[i]}"
 done
