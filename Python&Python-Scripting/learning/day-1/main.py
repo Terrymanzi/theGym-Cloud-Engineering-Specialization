322214f8f935af90cc064484222fbf535ad8e13d
terrymanzi <m.terry@alustudent.com>
2026-05-19 20:39:31 +0200

    feat: added basic Python program about basic variables and print statements

---
+++ Python&Python-Scripting/learning/day-1/main.py
@@ -0,0 +1,24 @@
+# this is my first python program
+print("I like pizaa!")
+print("it is really good!")
+
+# variables (basic types: integer, strings, boolean and float)
+
+full_name= "terry manzi"
+age = 20
+gpa= 4.5
+is_student = True
+print(f"Hello, my names are: {full_name}")
+print(f"I'm {age} years old")
+print(f"My current gpa is: {gpa}")
+
+if is_student:
+    print("I'm a student")
+else:
+    print("I'm not a student")
+
+# Arithmetics: 
+#       addition:       + 
+#       subtraction:    -
+#       Multplication:  *
+#       addition:       +
